9d7e9a9fd35dd14f5ee34d6f3af30dfc1aff4432
popcube <40394657+popcube@users.noreply.github.com>
2024-04-13 12:22:10 +0900

    csv upload check

--- main.py
+++ main.py
@@ -143,7 +143,8 @@ if __name__ == '__main__':
   added_ids = [id for id in new_ids if id not in cur_ids]
   
   # when new post is detected
-  if len(added_ids) > 0:
+  # if len(added_ids) > 0:
+  if True:
     
     added_ids.sort(reverse=True)
     send_to_discord(added_ids)
